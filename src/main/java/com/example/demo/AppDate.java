363c2bb324d956ab7fabee8cfd2b1aed34cb11bc
zget <dgetahun84@gmail.com>
2018-03-07 10:01:28 -0500

    Added Zodic and Swtich statments

--- src/main/java/com/example/demo/AppDate.java
+++ src/main/java/com/example/demo/AppDate.java
@@ -14,11 +14,14 @@ public class AppDate {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private long id;
 
+    //@DateTimeFormat(pattern="yyyy-MM-dd")
     private Date inputDate;
 
     private String mName;
 
     private String fName;
+
+    private String zodiac;
 
     @ManyToMany(fetch = FetchType.EAGER)
     private Set<AppUser> dateUser;
@@ -68,5 +71,13 @@ public class AppDate {
 
     public void setDateUser(Set<AppUser> dateUser) {
         this.dateUser = dateUser;
+    }
+
+    public String getZodiac() {
+        return zodiac;
+    }
+
+    public void setZodiac(String zodiac) {
+        this.zodiac = zodiac;
     }
 }
